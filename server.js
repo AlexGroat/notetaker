2d6a188fb06404e6264e605deefbf58a0b12b0a8
Alex Groat <alex.groat.95@gmail.com>
2021-11-21 23:55:51 +1030

    Server.js

--- server.js
+++ server.js
@@ -13,5 +13,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.use('./api', api);
+
+// access to all contents in public folder
+app.use(express.static('public'))
+
+// GET method route for homepage
+app.get('/', (req, res) =>
+ res.sendFile(path.join(__dirname, './public/index.html'))
+ );
+
+ // GET method route for notes
+ app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// wildcard method route which will redirect back to the index.html
+app.get('*', (req, res) => res.redirect('/'));
+
 
 app.listen(PORT, () =>  console.log(`This application is listening on server${PORT}`));
