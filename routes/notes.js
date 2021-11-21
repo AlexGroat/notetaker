9bb14f1d469dd090e4cbfd1ba59c1872c2977500
Alex Groat <alex.groat.95@gmail.com>
2021-11-21 23:42:32 +1030

    methods added

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,15 @@ const { v4:uuidv4 } = require('uuid');
 const note = require('express').Router();
 
 // GET method for retrieving all the notes in db.json
+note.get('/', (req, res) => {
 
+})
 // POST method for creating and adding a new note to the note array
+note.post('/', (req, res) => {
+    
+})
 
 // DELETE method (bonus) for removing a note from the array
+note.delete('/:id', (req, res) => {
+    
+})
