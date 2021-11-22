77e589643faf2088e168601a88f8172d3cc96160
Alex Groat <alex.groat.95@gmail.com>
2021-11-22 12:03:54 +1030

    update

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
-// import modules and packages
+
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index');
+const api = require('./routes/index.js');
 
-// port for heroku 
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-
-// middleware here
+ 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('./api', api);
+//route to get API
+app.use('/api', api);
 
-// access to all contents in public folder
-app.use(express.static('public'))
 
-// GET method route for homepage
+app.use(express.static('public'));
+
+
 app.get('/', (req, res) =>
- res.sendFile(path.join(__dirname, './public/index.html'))
- );
-
- // GET method route for notes
- app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
-// wildcard method route which will redirect back to the index.html
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
 app.get('*', (req, res) => res.redirect('/'));
 
 
-app.listen(PORT, () =>  console.log(`This application is listening on server${PORT}`));
+app.listen(PORT, () =>
+    console.log(`Application listening at http://localhost:${PORT}`)
+);
