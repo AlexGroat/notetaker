05d25a7969207967da011ed5b70091ffd3d0de2b
Alex Groat <alex.groat.95@gmail.com>
2021-11-22 12:05:32 +1030

    working deployed application

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
-
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,22 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 //route to get API
 app.use('/api', api);
 
-
 app.use(express.static('public'));
-
 
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
-
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-
 app.get('*', (req, res) => res.redirect('/'));
-
 
 app.listen(PORT, () =>
     console.log(`Application listening at http://localhost:${PORT}`)
